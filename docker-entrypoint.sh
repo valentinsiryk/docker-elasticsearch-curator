199676acd7ae8def04f905af1a21c563f18334ad
Valentin Siryk <valentinsiryk@gmail.com>
2018-07-12 15:04:44 +0300

    initial commit

---
+++ docker-entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+echo "[OK] Service was started"
+
+CURATOR_INTERVAL="${CURATOR_INTERVAL:-24h}"
+
+while true; do
+	echo "[INFO] Starting regular Elastic Curator job..."
+	/usr/bin/curator --config /curator.yml $@ /action.yml
+	echo "[INFO] Next execution after $CURATOR_INTERVAL"
+	sleep $CURATOR_INTERVAL
+done
