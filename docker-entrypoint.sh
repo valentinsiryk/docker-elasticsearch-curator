361424e2beb7e5b534b42122e3f29af783278388
Valentin Siryk <valentinsiryk@gmail.com>
2018-07-12 15:30:11 +0300

    hotfix

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,7 +6,7 @@ CURATOR_INTERVAL="${CURATOR_INTERVAL:-24h}"
 
 while true; do
 	echo "[INFO] Starting regular Elastic Curator job..."
-	/usr/bin/curator --config /curator.yml $@ /action.yml
+	/usr/bin/curator --config /curator.yml $@ /action.yml || exit 1
 	echo "[INFO] Next execution after $CURATOR_INTERVAL"
 	sleep $CURATOR_INTERVAL
 done
